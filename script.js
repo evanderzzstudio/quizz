00407068f6eab95a68c4e9aec36584fb59451eb5
evanderzzstudio <evanderstudio@gmail.com>
2023-11-27 05:14:03 +0700

    - Refactor form submission event listener.

--- script.js
+++ script.js
@@ -12,12 +12,20 @@ const questionItems = document.querySelectorAll(".question-item")
 const alert = document.querySelector("#alert")
 
 // TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behaviour
+form.addEventListener("submit", e => {
 
+  //    1. Prevent the default behaviour
+  e.preventDefault()
+  //    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
+  const checkedAnswers = answers.filter(answer => answer.checked)
+  //    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
+  checkedAnswers.forEach(answer => {
+    const isCorrect = answer.value === "true"
+    const questionItem = answer.closest(".question-item")
+  })
+  //    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
+  //    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
+  //    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
+  //    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
 
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+})
