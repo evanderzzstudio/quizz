1a338d727c2a1328c055b2e8cec1dfc6c7533849
evanderzzstudio <evanderstudio@gmail.com>
2023-11-27 05:27:38 +0700

    - Update the logic for adding and removing classes to question items based on correctness.

--- script.js
+++ script.js
@@ -24,7 +24,13 @@ form.addEventListener("submit", e => {
     const questionItem = answer.closest(".question-item")
   })
   //    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-  //    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
+  if (isCorrect) {
+    questionItem.classList.add("correct")
+    questionItem.classList.remove("incorrect")
+  } else {
+    //    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
+  }
+
   //    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
   //    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
 
